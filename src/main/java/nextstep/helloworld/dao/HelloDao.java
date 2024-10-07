2908fd64ec475334a235becdc465ab65351ac517
jution113 <jution113@gmail.com>
2024-10-07 14:09:11 +0900

    feat: HelloDao 생성

--- src/main/java/nextstep/helloworld/dao/HelloDao.java
+++ src/main/java/nextstep/helloworld/dao/HelloDao.java
@@ -1,2 +1,23 @@
-package nextstep.helloworld.dao;public class HelloDao {
+package nextstep.helloworld.dao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class HelloDao {
+    private JdbcTemplate jdbcTemplate;
+
+    public HelloDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void insert(String name) {
+        String SQL = "INSERT INTO hello (name) VALUES (?)";
+        jdbcTemplate.update(SQL, new Object[] {name});
+    }
+
+    public int countByNames(String name) {
+        String sql = "SELECT COUNT (*) FROM hello WHERE name = ? ";
+        return jdbcTemplate.queryForObject(sql, Integer.class, name);
+    }
 }

b64569ac5d0242f1b475a8bd937d2b37d7af55b2
jution113 <jution113@gmail.com>
2024-10-07 14:10:38 +0900

    feat: HelloController에 HelloDao에 생성자 주입

--- src/main/java/nextstep/helloworld/controller/HelloController.java
+++ src/main/java/nextstep/helloworld/controller/HelloController.java
@@ -1,11 +1,18 @@
 package nextstep.helloworld.controller;
 
+import nextstep.helloworld.dao.HelloDao;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private HelloDao helloDao;
+
+    public HelloController(HelloDao helloDao) {
+        this.helloDao = helloDao;
+    }
+
     @GetMapping("/hello")
     public String hello(@RequestParam(defaultValue = "") String name) {
         return "Hello " + name;

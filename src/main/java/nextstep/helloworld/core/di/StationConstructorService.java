2e1387ffc820d4a4f2834e0198a383c0d6dc36d6
yunuo46 <yunuo468@gmail.com>
2024-10-11 16:24:06 +0900

    feat: core mission complete

--- src/main/java/nextstep/helloworld/core/di/StationConstructorService.java
+++ src/main/java/nextstep/helloworld/core/di/StationConstructorService.java
@@ -1,8 +1,14 @@
 package nextstep.helloworld.core.di;
+import org.springframework.stereotype.Service;
 
+@Service
 public class StationConstructorService {
-    private StationRepository stationRepository;
+    private final StationRepository stationRepository;
 
+    // 생성자 주입
+    public StationConstructorService(StationRepository stationRepository) {
+        this.stationRepository = stationRepository;
+    }
     public String sayHi() {
         return stationRepository.sayHi();
     }
--- src/main/java/nextstep/helloworld/core/di/StationSetterService.java
+++ src/main/java/nextstep/helloworld/core/di/StationSetterService.java
@@ -1,8 +1,15 @@
 package nextstep.helloworld.core.di;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class StationSetterService {
     private StationRepository stationRepository;
 
+    @Autowired
+    public void setStationRepository(StationRepository stationRepository) {
+        this.stationRepository = stationRepository;
+    }
     public String sayHi() {
         return stationRepository.sayHi();
     }

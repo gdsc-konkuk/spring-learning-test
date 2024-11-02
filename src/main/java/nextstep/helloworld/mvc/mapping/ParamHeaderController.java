bc264a9b89a30b288f286909011c334cb2bdf0cd
WhitePiano <startoflateness@gmail.com>
2024-11-02 11:51:37 +0900

    fix(mapping): make path var, qs, header matched

--- src/main/java/nextstep/helloworld/mvc/mapping/ParamHeaderController.java
+++ src/main/java/nextstep/helloworld/mvc/mapping/ParamHeaderController.java
@@ -2,22 +2,27 @@ package nextstep.helloworld.mvc.mapping;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/param-header")
 public class ParamHeaderController {
 
+    @GetMapping("/message")
     public ResponseEntity<String> message() {
         return ResponseEntity.ok().body("message");
     }
 
-    public ResponseEntity<String> messageForParam() {
-        return ResponseEntity.ok().body("hello");
+    @GetMapping(value = "/message", params = {"name"})
+    public ResponseEntity<String> messageForParam(@RequestParam String name) {
+        return ResponseEntity.ok().body(name);
     }
 
-    public ResponseEntity<String> messageForHeader() {
-        return ResponseEntity.ok().body("hi");
+    @GetMapping(value = "/message", headers = {"HEADER"})
+    public ResponseEntity<String> messageForHeader(@RequestHeader String HEADER) {
+        return ResponseEntity.ok().body(HEADER);
     }
 }

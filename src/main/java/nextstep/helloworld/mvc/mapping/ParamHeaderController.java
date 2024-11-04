bd95fe515d74860efa1561b6a451aa339e51786f
jution113 <jution113@gmail.com>
2024-11-04 21:24:33 +0900

    feat: add mapping annotation to ParamHeaderController class

--- src/main/java/nextstep/helloworld/mvc/mapping/ParamHeaderController.java
+++ src/main/java/nextstep/helloworld/mvc/mapping/ParamHeaderController.java
@@ -3,20 +3,22 @@ package nextstep.helloworld.mvc.mapping;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/param-header")
 public class ParamHeaderController {
-
+    @GetMapping(value = "/message")
     public ResponseEntity<String> message() {
         return ResponseEntity.ok().body("message");
     }
 
-    public ResponseEntity<String> messageForParam() {
+    @GetMapping(value = "/message", params = "name")
+    public ResponseEntity<String> messageForParam(@RequestParam String name) {
         return ResponseEntity.ok().body("hello");
     }
-
+    @GetMapping(value = "message", headers = "HEADER")
     public ResponseEntity<String> messageForHeader() {
         return ResponseEntity.ok().body("hi");
     }
